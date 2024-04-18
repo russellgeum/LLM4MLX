43820cdf943c7cdc7458876a42923e0004db864e
Oppenheimer <49156892+russellgeum@users.noreply.github.com>
2024-04-18 22:49:07 +0900

    240418 최초 파일 업로드
    패키지 의존성, tutorial Llama2, Gemma 업로드

---
+++ requirements.sh
@@ -0,0 +1,7 @@
+pip3 install -q -U bitsandbytes==0.42.0
+pip3 install -q -U peft==0.8.2
+pip3 install -q -U trl==0.7.10
+pip3 install -q -U accelerate==0.27.1
+pip3 install -q -U datasets==2.17.0
+pip3 install -q -U transformers==4.38.1
+pip3 install wandb
